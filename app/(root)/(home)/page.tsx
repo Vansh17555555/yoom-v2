3ac446417b25543f9e0b4f7707cba1775a055d5a
Bhanu <bhanusngh12345@gmail.com>
2024-07-04 21:03:01 +0530

    fix : ui change

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,35 +1,9 @@
-import MeetingModal from '@/components/MeetingModal';
-import MeetingTypeList from '@/components/MeetingTypeList';
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-  const now = new Date();
-
-  const formatTime = (date:Date) => {
-    return date.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  };
-
-  const formatDate = (date:Date) => {
-    return new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' }).format(date);
-  };
-
-  const time = formatTime(now);
-  const date = formatDate(now);
-
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at 12:30 PM
-          </h2>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
-          </div>
-        </div>
-      </div>
-      <MeetingTypeList/>
+    <section className="flex size-full flex-col gap-10 text-white w-full h-full justify-center items-center">
+      <h1 className="text-[3rem] font-semibold">Landing Page</h1>
     </section>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import Mobilenav from './mobilenav'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Mobilenav from "./mobilenav";
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10 '>
-      <Link href='/' className='flex items-center gap-1'>
-        <Image src='/icons/logo.svg'width={32} height={32} alt="Yoom logo" className='max-sm:size-10'/>
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+    <nav className="flex-between fixed z-50  bg-[#1C1C1C] py-4 lg:px-10 w-full text-white">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
       </Link>
-      <div className='flex-between gap-5'>
-      <Mobilenav/>
+      <div className="flex-between gap-10">
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/upcoming">Upcoming</Link>
+        <Link href="/previous">Previous</Link>
+        <Link href="/recordings">Recordings</Link>
+        <Link href="/personal-room">Personal-Room</Link>
+        <Mobilenav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar 
+export default Navbar;
