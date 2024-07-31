92ab7706b237bcf322a2d19f05788007e2901061
Vansh Verma <vvansh739@gmail.com>
2024-07-31 21:38:33 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,24 +5,13 @@ import Mobilenav from "./mobilenav";
 import {SignedIn,UserButton} from "@clerk/nextjs"
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50  bg-[#1C1C1C] py-4 lg:px-10 w-full text-white">
-      <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/icons/logo.svg"
-          width={32}
-          height={32}
-          alt="Yoom logo"
-          className="max-sm:size-10"
-        />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Yoom
+      <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10 '>
+      <Link href='/' className='flex items-center gap-1'>
+        <Image src='/icons/logo.svg'width={32} height={32} alt="Yoom logo" className='max-sm:size-10'/>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom
         </p>
       </Link>
-      <div className="flex-between gap-10">
-        <Link href="/upcoming">Upcoming</Link>
-        <Link href="/previous">Previous</Link>
-        <Link href="/recordings">Recordings</Link>
-        <Link href="/personal-room">Personal-Room</Link>
+      <div className="flex-between gap-5">
         <Mobilenav />
       </div>
       <div className='flex-between gap-5'><SignedIn><UserButton/></SignedIn></div>
